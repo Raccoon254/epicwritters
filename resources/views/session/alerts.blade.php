58d4f355f347f46d71c64c5606a65066826ea658
Steve Tom <tomsteve187@gmail.com>
2023-10-24 12:29:05 +0300

    Added more changes to session alerts

--- resources/views/session/alerts.blade.php
+++ resources/views/session/alerts.blade.php
@@ -5,6 +5,24 @@
     </div>
 @endif
 
+@if(session('info'))
+    <div class="alert rounded mt-3 alert-info">
+        {{ session('info') }}
+    </div>
+@endif
+
+@if(session('warning'))
+    <div class="alert rounded mt-3 alert-warning">
+        {{ session('warning') }}
+    </div>
+@endif
+
+@if(session('message'))
+    <div class="alert rounded mt-3 alert-primary">
+        {{ session('message') }}
+    </div>
+@endif
+
 @if(session('error'))
     <div class="alert rounded-[0px] text-white border-0 mt-3 bg-red-500 alert-danger">
         {{ session('error') }}
