39ec76cd5e7f2623a63341285df2214ae23574bf
Steve Tom <tomsteve187@gmail.com>
2023-10-29 17:33:37 +0300

    Styled coming soon view

--- resources/views/coming-soon.blade.php
+++ resources/views/coming-soon.blade.php
@@ -8,8 +8,8 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
-                <div class="flex flex-col justify-center items-center">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-4">
+                <div class="flex flex-col gap-4 justify-center items-center">
                     <div class="text-4xl font-bold text-gray-800">
                         Coming Soon
                     </div>
