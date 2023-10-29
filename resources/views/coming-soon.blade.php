ae77f8cd6ab0e51db964281d7daac55ab2bdd34d
Steve Tom <tomsteve187@gmail.com>
2023-10-29 17:31:31 +0300

    Content for coming soon view

---
+++ resources/views/coming-soon.blade.php
@@ -0,0 +1,24 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Coming Soon
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
+                <div class="flex flex-col justify-center items-center">
+                    <div class="text-4xl font-bold text-gray-800">
+                        Coming Soon
+                    </div>
+                    <div class="text-xl text-gray-800">
+                        This feature is coming soon.
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</x-app-layout>
