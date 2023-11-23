83b08222ff015d3686c0e7e9727cae95ac26161c
Raccoon254 <tomsteve187@gmail.com>
2023-11-23 11:24:55 +0300

    Created dynamic admin cards

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,8 +7,26 @@ use Illuminate\View\View;
 
 class AdminController extends Controller
 {
-    public function dashboard(): View
-    {
-        return view('admin.dashboard');
-    }
+public function dashboard(): View
+{
+    $cards = [
+        [
+            'content' => 'Payments',
+            'icon' => 'fas fa-money-check-alt', // FontAwesome class for the icon
+            'route' => route('payments.index'),
+        ],
+        [
+            'content' => 'Verify Payment',
+            'icon' => 'fas fa-check-circle', // FontAwesome class for the icon
+            'route' => route('payments.verify', ['payment' => 1]), // replace 1 with actual payment id
+        ],
+        [
+            'content' => 'Dashboard',
+            'icon' => 'fas fa-tachometer-alt', // FontAwesome class for the icon
+            'route' => route('admin.dashboard'),
+        ],
+    ];
+
+    return view('admin.dashboard', ['cards' => $cards]);
+}
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,10 @@
 <x-app-layout>
     <section class="upper grid grid-cols-4">
-        <div class="card h-40 flex justify-center items-center shadow">
-            Hello
-        </div>
+        @foreach($cards as $card)
+            <a href="{{ $card['route'] }}" class="card h-40 flex justify-center items-center shadow">
+                <i class="{{ $card['icon'] }}"></i>
+                <p>{{ $card['content'] }}</p>
+            </a>
+        @endforeach
     </section>
 </x-app-layout>
