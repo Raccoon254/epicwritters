cfd40e2a8c6446c8d10c3881193a6df6211ac746
Raccoon254 <tomsteve187@gmail.com>
2023-11-23 11:31:47 +0300

    Created dynamic admin cards styles

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,17 +12,12 @@ public function dashboard(): View
     $cards = [
         [
             'content' => 'Payments',
-            'icon' => 'fas fa-money-check-alt', // FontAwesome class for the icon
+            'icon' => 'fas fa-money-check-alt',
             'route' => route('payments.index'),
         ],
         [
-            'content' => 'Verify Payment',
-            'icon' => 'fas fa-check-circle', // FontAwesome class for the icon
-            'route' => route('payments.verify', ['payment' => 1]), // replace 1 with actual payment id
-        ],
-        [
             'content' => 'Dashboard',
-            'icon' => 'fas fa-tachometer-alt', // FontAwesome class for the icon
+            'icon' => 'fas fa-tachometer-alt',
             'route' => route('admin.dashboard'),
         ],
     ];
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,8 +1,8 @@
 <x-app-layout>
-    <section class="upper grid grid-cols-4">
+    <section class="upper grid sm:grid-cols-2 md:grid-cols-4 gap-2">
         @foreach($cards as $card)
-            <a href="{{ $card['route'] }}" class="card h-40 flex justify-center items-center shadow">
-                <i class="{{ $card['icon'] }}"></i>
+            <a href="{{ $card['route'] }}" class="rounded-md h-40 flex flex-col ring-1 ring-offset-1 ring-inset justify-center items-center shadow card-hover hover:bg-accent">
+                <i class="{{ $card['icon'] }} fa-5x card-icon"></i>
                 <p>{{ $card['content'] }}</p>
             </a>
         @endforeach
