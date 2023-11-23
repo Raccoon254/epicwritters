7ef7d63434a41c08d8be656e47820d2278159aa0
Raccoon254 <tomsteve187@gmail.com>
2023-11-23 12:24:26 +0300

    Added events livewire code

--- app/Livewire/EventComponent.php
+++ app/Livewire/EventComponent.php
@@ -1,13 +1,82 @@
 <?php
 
-namespace App\Livewire;
+namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Event;
 
 class EventComponent extends Component
 {
+    public $events, $name, $description, $start_time, $end_time, $event_id;
+    public $isOpen = 0;
+
     public function render()
     {
-        return view('livewire.event-component');
+        $this->events = Event::all();
+        return view('livewire.events');
+    }
+
+    public function create()
+    {
+        $this->resetInputFields();
+        $this->openModal();
+    }
+
+    public function openModal()
+    {
+        $this->isOpen = true;
+    }
+
+    public function closeModal()
+    {
+        $this->isOpen = false;
+    }
+
+    private function resetInputFields(){
+        $this->name = '';
+        $this->description = '';
+        $this->start_time = '';
+        $this->end_time = '';
+        $this->event_id = '';
+    }
+
+    public function store()
+    {
+        $this->validate([
+            'name' => 'required',
+            'start_time' => 'required',
+            'end_time' => 'required',
+        ]);
+
+        Event::updateOrCreate(['id' => $this->event_id], [
+            'name' => $this->name,
+            'description' => $this->description,
+            'start_time' => $this->start_time,
+            'end_time' => $this->end_time
+        ]);
+
+        session()->flash('message',
+            $this->event_id ? 'Event Updated Successfully.' : 'Event Created Successfully.');
+
+        $this->closeModal();
+        $this->resetInputFields();
+    }
+
+    public function edit($id)
+    {
+        $event = Event::findOrFail($id);
+        $this->event_id = $id;
+        $this->name = $event->name;
+        $this->description = $event->description;
+        $this->start_time = $event->start_time;
+        $this->end_time = $event->end_time;
+
+        $this->openModal();
+    }
+
+    public function delete($id)
+    {
+        Event::find($id)->delete();
+        session()->flash('message', 'Event Deleted Successfully.');
     }
 }
--- resources/views/livewire/event-component.blade.php
+++ resources/views/livewire/event-component.blade.php
@@ -1,3 +1,44 @@
-<div>
-    {{-- The best athlete wants his opponent at his best. --}}
-</div>
+<x-app-layout>
+    <div>
+        @if(session()->has('message'))
+            <div class="alert alert-success">
+                {{ session()->get('message') }}
+            </div>
+        @endif
+
+        <button wire:click="create()" class="btn btn-primary">Create New Event</button>
+
+        @if($isOpen)
+            @include('livewire.create')
+        @endif
+
+        <table class="table table-striped mt-5">
+            <thead>
+            <tr>
+                <th>No.</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Start Time</th>
+                <th>End Time</th>
+                <th>Action</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($events as $event)
+                <tr>
+                    <td>{{ $event->id }}</td>
+                    <td>{{ $event->name }}</td>
+                    <td>{{ $event->description }}</td>
+                    <td>{{ $event->start_time }}</td>
+                    <td>{{ $event->end_time }}</td>
+                    <td>
+                        <button wire:click="edit({{ $event->id }})" class="btn btn-primary btn-sm">Edit</button>
+                        <button wire:click="delete({{ $event->id }})" class="btn btn-danger btn-sm">Delete</button>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+</x-app-layout>
+
