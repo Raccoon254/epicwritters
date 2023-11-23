3a83e7626dd3d549129c4b7d6ded8af36364522d
Raccoon254 <tomsteve187@gmail.com>
2023-11-23 14:14:03 +0300

    User enroll events components code create

--- app/Livewire/EventEnrollmentComponent.php
+++ app/Livewire/EventEnrollmentComponent.php
@@ -2,12 +2,34 @@
 
 namespace App\Livewire;
 
+use Illuminate\View\View;
 use Livewire\Component;
+use App\Models\Event;
+use Illuminate\Support\Facades\Auth;
 
 class EventEnrollmentComponent extends Component
 {
-    public function render()
+    public $events;
+
+    public function mount(): void
     {
-        return view('livewire.event-enrollment-component');
+        $this->events = Event::all();
+    }
+
+    public function render(): View
+    {
+        return view('livewire.event-enrollment');
+    }
+
+    public function enroll(Event $event): void
+    {
+        Auth::user()->events()->attach($event);
+        session()->flash('message', 'You have successfully enrolled for the event.');
+    }
+
+    public function unenroll(Event $event): void
+    {
+        Auth::user()->events()->detach($event);
+        session()->flash('message', 'You have successfully unenrolled from the event.');
     }
 }
--- resources/views/livewire/event-enrollment-component.blade.php
+++ resources/views/livewire/event-enrollment-component.blade.php
@@ -1,3 +1,15 @@
 <div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+    <div class="grid grid-cols-3 gap-4">
+        @foreach($events as $event)
+            <div class="card">
+                <h3>{{ $event->name }}</h3>
+                <p>{{ $event->description }}</p>
+                @if(Auth::user()->events->contains($event))
+                    <button wire:click="unenroll({{ $event->id }})" class="btn btn-danger">Unenroll</button>
+                @else
+                    <button wire:click="enroll({{ $event->id }})" class="btn btn-primary">Enroll</button>
+                @endif
+            </div>
+        @endforeach
+    </div>
 </div>
