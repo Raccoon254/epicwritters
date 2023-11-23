9dcfcfcaa9405b2166dc268b81ab4665c46000bd
Raccoon254 <tomsteve187@gmail.com>
2023-11-23 11:19:51 +0300

    Created admin cards

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,3 +1,7 @@
 <x-app-layout>
-
+    <section class="upper grid grid-cols-4">
+        <div class="card h-40 flex justify-center items-center shadow">
+            Hello
+        </div>
+    </section>
 </x-app-layout>
